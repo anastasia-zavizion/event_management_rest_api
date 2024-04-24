caa3fa7c0a08255955a2ed897af3a5fcbd572515
Nastya Zavizion <nastya.gorobets95@gmail.com>
2024-04-24 13:43:04 +0300

    Added optional relations for events

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -17,8 +17,23 @@ class EventController extends Controller
      */
     public function index()
     {
-        return EventResource::collection(Event::with(['user','attendees'])->paginate());
+        $relations = ['user','attendees', 'attendees.user'];
+        $query = Event::query();
+        foreach ($relations as $relation){
+            $query->when($this->shouldIncludeRelations($relation), fn($q)=>$q->with($relation));
+        }
+        return EventResource::collection($query->paginate());
     }
+
+    protected function shouldIncludeRelations(string $relation): bool{
+        $include = request()->query('include');
+        if(!$include){
+            return false;
+        }
+        $relations = array_map('trim',explode(',',$include));
+        return in_array($relation,$relations);
+    }
+
 
     /**
      * Store a newly created resource in storage.
