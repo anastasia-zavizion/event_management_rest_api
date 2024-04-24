1d63e724d6b0371d62a474ec0836c882dc338796
Nastya Zavizion <nastya.gorobets95@gmail.com>
2024-04-24 15:27:02 +0300

    Login method for create token

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $validated =  $request->validate([
+           'email'=>'required|email',
+           'password'=>'required'
+        ]);
+        if(Auth::attempt($validated)){
+            $user = Auth::user();
+            $token = $user->createToken('api-token')->plainTextToken;
+            return response()->json(['token'=>$token]);
+        }else{
+            throw ValidationException::withMessages([
+                'email'=>'Incorrect data'
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,3 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)->scoped()->except(['update']);
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
